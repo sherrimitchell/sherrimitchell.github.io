f667159154df546def15b4e0a401c3f11e19dfb2
Sherri Mitchell <rsamitchell@att.net>
2015-10-25 21:56:03 -0400

    Adding updates to portfolio: added font-awesome, images, wow.js, updated style sheet and js, added images.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,26 @@
 (function($){
   $(function(){
 
+// parallax
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+
+// scrollspy
+$('.scrollspy').scrollSpy();
+
+// collapsible pop-out
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+
+// slider
+	$('.slider').slider({full_width: true});
+		// Turn off indicators
+  
+ 
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
